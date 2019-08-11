20ae983f5654ba17aea98d57a473cad09f24e816
glebkiselev11 <12bny23gleb@gmail.com>
2019-08-11 21:26:24 +0300

    add preview image via vuejs

--- client/src/components/Form.vue
+++ client/src/components/Form.vue
@@ -30,9 +30,9 @@
     </div>
     <div class="form__image-window-wrap">
 		<label class="label_simple">Cover</label>
-		<div class="form__image-window" id="image-form"></div>
+		<div class="form__image-window" id="image-form" :style="cover"></div>
 		<label for="inputfile" class='btn-inputfile'>select a file</label>
-		<input name="cover" type="file" id="inputfile" class="inputfile" @change="previewFiles">
+		<input name="cover" type="file" id="inputfile" class="inputfile" @change="sync">
     </div>   
     <button type="submit" class='btn-submit'>Сreate template</button>
 	</form>
@@ -40,59 +40,26 @@
 
 <script>
 
-function handleFileSelect(evt) {
-  const files = evt.target.files;
-
-  // Loop through the FileList and render image files as thumbnails.
-  for (let i = 0, f; f = files[i]; i++) {
-
-    // Only process image files.
-    if (!f.type.match('image.*')) {
-      continue;
-    }
-
-    const reader = new FileReader();
-    
-
-    // Closure to capture the file information.
-    reader.onload = (function(theFile) {
-      return function(e) {
-        // Render thumbnail.
-        const imageWrap = document.querySelector('.form__image-window');
-        imageWrap.style.backgroundImage = `url('${e.target.result}')`;
-      };
-    })(f);
-
-    // Read in the image file as a data URL.
-    reader.readAsDataURL(f);
-  }
-}
-
-document.getElementById('inputfile').addEventListener('change', handleFileSelect, false);
-
 export default {
 	name: 'Form',
 	data() {
 		return {
 			album_name: "",
 			artist_name: "",
-			album_tracks: 0,
-			date_rel: "",
-			files: []
-			
+			album_tracks: 2,
+            date_rel: "",
+            cover: ""			
 		}
-	},
+    },
+    
 	methods: {
-		previewFiles() {
-			this.files = this.$refs.myFiles.files
-		},
 		toast(err) {
 			this.$toasted.show(err, {
 						theme: "outline", 
 						position: "top-center", 
 						duration : 3000
 				})
-		},
+        },
 		onSubmit() {
 			if (!this.album_name) {
 				this.toast("Please enter album name")
@@ -102,13 +69,37 @@ export default {
 				this.toast("Please enter the number of tracks")
 			} else if (!this.date_rel) {
 				this.toast("Please enter release date")
-			} else if (!this.files) {
-				this.toast("Please upload cover")
-			}
-			
-				
-		}
-	}
+			} 			
+        },
+        selectImage (file) {
+            this.file = file;
+            let reader = new FileReader();
+            reader.onload = this.onImageLoad;
+            reader.readAsDataURL(file);
+        },
+        sync (e) {
+            e.preventDefault();
+            this.selectImage(e.target.files[0]);
+        },
+
+        onImageLoad (e) {
+            this.content = e.target.result;
+            let filename = this.file instanceof File ? this.file.name : '';
+            this.$emit('input', filename);
+            this.cover = `background-image: url("${this.content}")`
+            this.$emit('image-changed', this.content);
+        }
+            
+    },
+    computed: {
+        src () {
+            if (this.content) {
+                return this.content;
+            }
+            return this.isEmpty ? '' : this.srcPrefix + this.value;
+
+        }
+    },
 }
 
 </script>
