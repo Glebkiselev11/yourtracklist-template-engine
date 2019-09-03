eebbe66df54cb7e24de5e2aa941a41db4842434b
glebkiselev11 <12bny23gleb@gmail.com>
2019-09-03 12:40:10 +0300

    release word checking added

--- routes/releases.js
+++ routes/releases.js
@@ -25,21 +25,38 @@ const upload = multer({ storage });
 router.post('/', upload.single('cover'), async (req,res) => {
     const { album_name, artist_name, album_tracks, date_rel, type} = req.body;
 
+    console.log(artist_name.length)
+
 
     Jimp.read('templateImage/template_rel.jpg', (err, template) => {
         Jimp.read('uploads/cover.jpg', (err, image) => {
             if (err) throw err;
             image.cover(192, 192) // resize
             template.composite(image, 304, 4 ).write('result/cart.jpg'); 
-            
-                Jimp.loadFont('fontsForCart/gotham_bold_24.fnt').then(font => {
+
+                // В зависимости от длины приходящего слова - подставляет нужный размер шрифта, 
+                // чтобы текст не налез на фото
+                Jimp.loadFont(
+                    album_name.length < 23 ? 'fontsForCart/gotham_bold_24.fnt' : 
+                    album_name.length < 26 ? 'fontsForCart/gotham_bold_21.fnt' : 
+                    album_name.length < 29 ? 'fontsForCart/gotham_bold_19.fnt' :
+                    album_name.length < 32 ? 'fontsForCart/gotham_bold_17.fnt' :
+                    album_name.length < 35 ? 'fontsForCart/gotham_bold_15.fnt' : 
+                    'fontsForCart/gotham_bold_13.fnt' ).then(font => {
                     // load font from .fnt file 
                     template
                     .print(font, 20, 21, album_name)
                     .write('result/cart.jpg'); 
                 });
 
-                Jimp.loadFont('fontsForCart/gotham_light_24.fnt').then(font => {
+                // Здесь тоже самое
+                Jimp.loadFont(
+                    artist_name.length < 21 ?'fontsForCart/gotham_light_24.fnt':
+                    artist_name.length < 23 ?'fontsForCart/gotham_light_21.fnt':
+                    artist_name.length < 25 ?'fontsForCart/gotham_light_19.fnt':
+                    artist_name.length < 27 ?'fontsForCart/gotham_light_17.fnt':
+                    artist_name.length < 29 ?'fontsForCart/gotham_light_15.fnt': 
+                    'fontsForCart/gotham_light_13.fnt').then(font => {
                     // load font from .fnt file 
                     template
                     .print(font, 21, 52, `by ${artist_name}`)
