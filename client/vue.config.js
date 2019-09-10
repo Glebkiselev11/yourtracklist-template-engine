0092e2ac4b6c2315568ef4c624d5733d6516d3df
glebkiselev11 <12bny23gleb@gmail.com>
2019-09-11 01:52:59 +0300

    fix error for heroku

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,4 +2,14 @@ const path = require('path');
 
 module.exports = {
     outputDir: path.resolve(__dirname, '../public'),
+    devServer: {
+        proxy: {
+            '/releases/create': {
+                target: 'http://localhost:5000'
+            },
+            '/result': {
+                target: 'http://localhost:5000'
+            }
+        }
+    }
 }
